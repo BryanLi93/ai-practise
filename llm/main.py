a6a128a287b4b912d6437b7baae9ba62f37ec159
sujian.li <sujian.li@ctg.cn>
2026-04-09 16:10:42 +0800

    feat: 添加流式聊天功能，支持用户输入并实时返回助手回复

---
+++ llm/main.py
@@ -0,0 +1,54 @@
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_BASE_URL = os.getenv("GOOGLE_BASE_URL")
+
+client = OpenAI(base_url=GOOGLE_BASE_URL, api_key=GOOGLE_API_KEY)
+messages: list[ChatCompletionMessageParam] = [{"role": "system", "content": "你是一个助手"}]
+
+# while True:
+#     user_input = input("You: ")
+#     if user_input in ["quit", "exit"]:
+#         break
+#     try:
+#         response = client.chat.completions.create(
+#             model="gemini-3.1-flash-lite-preview",
+#             messages=messages
+#         )
+#         reply = response.choices[0].message.content
+#         if reply:
+#             messages.append({ "role": "user", "content": user_input })
+#             messages.append({ "role": "assistant", "content": reply })
+#             print(f"Assistant: {reply}")
+#         else:
+#             print("error: LLM no response")
+#     except Exception as e:
+#         print(e)
+
+# 流式
+while True:
+    user_input = input("You: ")
+    if user_input in ["quit", "exit"]:
+        break
+    try:
+        messages.append({ "role": "user", "content": user_input })
+        stream = client.chat.completions.create(
+            model="gemini-3.1-flash-lite-preview",
+            messages=messages,
+            stream=True
+        )
+        reply = ""
+        print("\nAssistant: ")
+        for chunk in stream:
+            delta = chunk.choices[0].delta
+            if delta.content:
+                reply += delta.content
+                print(delta.content, end="", flush=True)
+        messages.append({ "role": "assistant", "content": reply })
+        print("\n")
+    except Exception as e:
+        print(e)
